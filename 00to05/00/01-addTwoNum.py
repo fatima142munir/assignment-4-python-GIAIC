a22dc535ba0a582ef0fa9fadf3bf770a11013ca9
Fatima Munir <fmunir142@gmail.com>
2025-04-16 16:21:40 +0500

    assignment 4 started

---
+++ 00to05/00/01-addTwoNum.py
@@ -0,0 +1,11 @@
+def main():
+    num1 : int = int(input("Enter first number: "))
+    
+    num2  : int = int(input("Enter second number: "))
+    
+    total : int = num1 + num2
+    print(f"Sum of {num1} and {num2} is {total}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ 00to05/00/02-agreementBot.py
@@ -0,0 +1,7 @@
+def main():
+    userInput : str = input("Who is your favorite animal: ")
+    print(f"My favorite animal is also {userInput}!")
+
+
+if __name__ == '__main__':
+    main()
